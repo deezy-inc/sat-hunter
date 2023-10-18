acd5a5b96ce7fbc9850a6bc2cd7fc9b28ed64f90
Danny Diekroeger <dannydiek@gmail.com>
2023-10-18 16:09:53 -0700

    psbt finalize

--- wallet.js
+++ wallet.js
@@ -65,7 +65,7 @@ function sign_transaction({ psbt }) {
     }
     let psbt_object = bitcoin.Psbt.fromBase64(psbt)
     psbt_object.updateInput(0, {
-        tapInternalKey: TWEAKED_CHILD_NODE.publicKey,
+        tapInternalKey: Buffer.from(TWEAKED_CHILD_NODE.publicKey.toString('hex'), 'hex')
     })
     console.log(TWEAKED_CHILD_NODE.publicKey.toString('hex'))
     psbt_object = psbt_object.signInput(0, TWEAKED_CHILD_NODE, [bitcoin.Transaction.SIGHASH_ALL])
