e7841a6637eb1d2fe2baecaa5c98bf08c2298c00
Danny Diekroeger <dannydiek@gmail.com>
2023-10-18 09:03:40 -0700

    withdraw test

---
+++ tests/withdraw.js
@@ -0,0 +1,21 @@
+const exchanges = require('../exchanges/config.js')
+const EXCHANGE = process.argv[2]
+const AMOUNT_BTC = parseFloat(process.argv[3])
+if (!EXCHANGE) {
+    throw new Error('exchange must be set')
+}
+if (!exchanges[EXCHANGE]) {
+    throw new Error(`${EXCHANGE} is not a valid exchange. Available options are ${Object.keys(exchanges).join(', ')}`)
+}
+if (!AMOUNT_BTC) {
+    throw new Error('amount must be set')
+}
+
+async function run() {
+    const exchange = exchanges[EXCHANGE]
+    const resp = await exchange.withdraw({ amount_btc: AMOUNT_BTC })
+    console.log(resp)
+    process.exit(0)
+}
+
+run()
