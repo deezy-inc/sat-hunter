dcf42616e1620c2852f21a1cb806e419a9e011cf
Danny Diekroeger <dannydiek@gmail.com>
2023-10-20 14:50:27 -0700

    perfect next block min fee rate

--- fees.js
+++ fees.js
@@ -6,6 +6,9 @@ if (FEE_PREF !== 'high' && FEE_PREF !== 'medium' && FEE_PREF !== 'low') {
 }
 // Get fee rate from mempool.space
 async function get_fee_rate() {
+    if (process.env.AUTO_RBF) {
+        return get_min_next_block_fee_rate()
+    }
     const { data } = await axios.get('https://mempool.space/api/v1/fees/recommended').catch(err => {
         console.error(err)
         return { data: {} }
@@ -14,6 +17,17 @@ async function get_fee_rate() {
     if (FEE_PREF === 'medium') return data.halfHourFee
     if (FEE_PREF === 'low') return data.hourFee
     throw new Error('FEE_PREFERENCE must be one of: high, medium, low')
+}
+
+async function get_min_next_block_fee_rate() {
+    const { data } = await axios.get('https://mempool.space/api/v1/fees/mempool-blocks').catch(err => {
+        console.error(err)
+        return { }
+    })
+    if (!data) {
+        throw new Error('Could not get mempool blocks')
+    }
+    return data[0].feeRange[1]
 }
 
 module.exports = {
