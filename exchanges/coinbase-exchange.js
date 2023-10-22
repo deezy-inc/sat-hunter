437ce7e2768da7d1298d3d3e3220d4399019af3d
Danny Diekroeger <dannydiek@gmail.com>
2023-10-22 08:56:21 -0700

    coinbase exchange api

---
+++ exchanges/coinbase-exchange.js
@@ -0,0 +1,94 @@
+const axios = require('axios')
+const crypto = require('crypto')
+const totp = require("totp-generator")
+const BASE_URL = 'https://api.exchange.coinbase.com'
+
+let BTC_ACCOUNT_ID = null
+
+function create_signature({ path, timestamp, method, body = ''}) {
+    const data = `${timestamp}${method}${path}${body}`
+    return crypto.createHmac('sha256', process.env.COINBASE_EXCHANGE_API_SECRET)
+        .update(data)
+        .digest('hex');
+}
+function create_headers({ path, timestamp, method, body = '' }) {
+    return {
+        'CB-ACCESS-KEY': process.env.COINBASE_EXCHANGE_API_KEY,
+        'CB-ACCESS-TIMESTAMP': timestamp,
+        'CB-ACCESS-SIGN': create_signature({ path, timestamp, method, body }),
+        'CB-VERSION': '2023-10-19'
+    }
+}
+async function get_btc_account_id() {
+    const path = '/accounts'
+    const timestamp = `${Math.floor(Date.now() / 1000)}`
+    const method = 'GET'
+    const headers = create_headers({ path, timestamp, method })
+    const { data } = await axios.get(`${BASE_URL}${path}`, { headers }).catch(err => {
+        console.log(err)
+        return {}
+    })
+    const wallet_info = data.filter(it => it.currency === 'BTC')
+    return wallet_info[0].id
+}
+
+async function get_btc_balance() {
+    if (!process.env.COINBASE_EXCHANGE_API_KEY || !process.env.COINBASE_EXCHANGE_API_SECRET) {
+        throw new Error('COINBASE_EXCHANGE_API_KEY and COINBASE_EXCHANGE_API_SECRET must be set')
+    }
+    if (!BTC_ACCOUNT_ID) {
+        BTC_ACCOUNT_ID = await get_btc_account_id()
+    }
+    const path = `/accounts/${BTC_ACCOUNT_ID}`
+    const timestamp = `${Math.floor(Date.now() / 1000)}`
+    const method = 'GET'
+    const headers = create_headers({ path, timestamp, method })
+    const { data } = await axios.get(`${BASE_URL}${path}`, { headers }).catch(err => {
+        console.log(err)
+        return {}
+    })
+    return parseFloat(data.balance)
+}
+
+async function withdraw({ amount_btc }) {
+    if (!process.env.COINBASE_EXCHANGE_API_KEY || !process.env.COINBASE_EXCHANGE_API_SECRET) {
+        throw new Error('COINBASE_EXCHANGE_API_KEY and COINBASE_EXCHANGE_API_SECRET must be set')
+    }
+    if (!process.env.COINBASE_EXCHANGE_WITHDRAWAL_ADDRESS) {
+        throw new Error('COINBASE_WITHDRAW_ADDRESS must be set')
+    }
+    if (!BTC_ACCOUNT_ID) {
+        BTC_ACCOUNT_ID = await get_btc_account_id()
+    }
+    const path = `/withdrawals/crypto`
+    const timestamp = `${Math.floor(Date.now() / 1000)}`
+    const method = 'POST'
+    const body = {
+        amount: `${amount_btc}`,
+        currency: 'BTC',
+        profile_id: BTC_ACCOUNT_ID,
+        crypto_address: process.env.COINBASE_EXCHANGE_WITHDRAWAL_ADDRESS,
+        add_network_fee_to_total: false,
+        two_factor_code: totp(process.env.COINBASE_EXCHANGE_TOTP_SECRET)
+    }
+    const headers = create_headers({ path, timestamp, method, body: JSON.stringify(body) })
+    headers['Content-Type'] = 'application/json'
+    const { data } = await axios.post(`${BASE_URL}${path}`, body, { headers }).catch(err => {
+        console.log(err.response.data)
+        throw new Error(JSON.stringify(err.response.data, null, 2))
+    })
+    console.log(data)
+}
+
+async function get_deposit_address() {
+    if (!process.env.COINBASE_EXCHANGE_DEPOSIT_ADDRESS) {
+        throw new Error('COINBASE_EXCHANGE_DEPOSIT_ADDRESS must be set')
+    }
+    return process.env.COINBASE_EXCHANGE_DEPOSIT_ADDRESS
+}
+
+module.exports = {
+    get_btc_balance,
+    withdraw,
+    get_deposit_address
+}
--- exchanges/config.js
+++ exchanges/config.js
@@ -1,9 +1,10 @@
 const bitfinex = require('./bitfinex')
 const kraken = require('./kraken')
 const coinbase = require('./coinbase')
+const coinbase_exchange = require('./coinbase-exchange')
 const gemini = require('./gemini')
 const binance = require('./binance')
 
 module.exports = {
-  bitfinex, kraken, coinbase, gemini, binance
+  bitfinex, kraken, coinbase, gemini, binance, coinbase_exchange
 }
