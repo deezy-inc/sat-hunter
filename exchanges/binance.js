75c60b059a02a90222728cc4c1226f3a4bebe3c8
Danny Diekroeger <dannydiek@gmail.com>
2023-10-28 08:08:16 -0700

    handle str or num for binance amount

--- exchanges/binance.js
+++ exchanges/binance.js
@@ -32,7 +32,7 @@ async function withdraw({ amount_btc }) {
     const { data } = await client.withdraw({
         coin: 'BTC',
         address: process.env.BINANCE_WITHDRAWAL_ADDRESS,
-        amount: parseFloat(amount_btc.toFixed(8))
+        amount: parseFloat(parseFloat(amount_btc).toFixed(8))
     }).catch(err => {
         console.log(err)
         throw new Error(err.message)
