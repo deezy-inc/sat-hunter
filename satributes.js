952f83cdbb63bf5bf2c3ed0375fc29eedb842cd5
Danny Diekroeger <dannydiek@gmail.com>
2023-10-28 07:56:43 -0700

    Fix year log

--- satributes.js
+++ satributes.js
@@ -28,7 +28,7 @@ function generate_satributes_messages(satributes) {
         for (const inscription_id of satribute.inscriptions || []) {
             msg += `\n${INSCRIPTION_BASE_URL}/${inscription_id}`
         }
-        msg += `\n#${satribute.sat_number}${satribute.name ? `\nName: ${satribute.name}` : ''}${satribute.year ? `Year: ${satribute.year}` : ''}`
+        msg += `\n#${satribute.sat_number}${satribute.name ? `\nName: ${satribute.name}` : ''}${satribute.year ? `\nYear: ${satribute.year}` : ''}`
         messages.push(msg)
     }
     return messages
