6d1e7ad289037dd77c6afc4c9e4c8e6ddcb04ed9
Danny Diekroeger <dannydiek@gmail.com>
2023-11-03 11:15:01 -0700

    logging

--- index.js
+++ index.js
@@ -247,6 +247,7 @@ async function run() {
 function get_excluded_tags({ fee_rate }) {
     let configured_excluded_tags = process.env.EXCLUDE_TAGS
     if (process.env.EXCLUDE_TAGS_HIGH_FEE_THRESHOLD && fee_rate > parseFloat(process.env.EXCLUDE_TAGS_HIGH_FEE_THRESHOLD)) {
+        console.log(`Using high fee excluded tags`)
         configured_excluded_tags = process.env.EXCLUDE_TAGS_HIGH_FEE
     }
     if (configured_excluded_tags === "") {
