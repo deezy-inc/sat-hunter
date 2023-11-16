375cd205954aa3c677f5abaae99a9f8588d0e6c5
Danny Diekroeger <dannydiek@gmail.com>
2023-11-16 15:34:29 -0800

    MIN_FEE_BUFFER_PERCENT

--- fees.js
+++ fees.js
@@ -4,6 +4,17 @@ const FEE_PREF = process.env.FEE_PREFERENCE || 'medium'
 if (FEE_PREF !== 'high' && FEE_PREF !== 'medium' && FEE_PREF !== 'low') {
     throw new Error('FEE_PREFERENCE must be one of: high, medium, low')
 }
+
+if (process.env.MIN_FEE_BUFFER && process.env.MIN_FEE_BUFFER_PERCENT) {
+    throw new Error('MIN_FEE_BUFFER and MIN_FEE_BUFFER_PERCENT cannot both be set')
+}
+let MIN_FEE_BUFFER_PERCENT
+if (process.env.MIN_FEE_BUFFER_PERCENT) {
+    MIN_FEE_BUFFER_PERCENT = parseFloat(process.env.MIN_FEE_BUFFER_PERCENT)
+    if (MIN_FEE_BUFFER_PERCENT < 1 || MIN_FEE_BUFFER_PERCENT > 2) {
+        console.log(`MIN_FEE_BUFFER_PERCENT must be between 1.00 and 2.00`)
+    }
+}
 const MIN_FEE_BUFFER = parseFloat(process.env.MIN_FEE_BUFFER || 3)
 // Get fee rate from mempool.space
 async function get_fee_rate() {
@@ -27,6 +38,10 @@ async function get_min_next_block_fee_rate() {
     })
     if (!data) {
         throw new Error('Could not get mempool blocks')
+    }
+    if (MIN_FEE_BUFFER_PERCENT) {
+        const min_fee_buffer_percent = parseFloat(process.env.MIN_FEE_BUFFER_PERCENT)
+        return Math.round((data[0].feeRange[0] * min_fee_buffer_percent) * 10) / 10
     }
     // We add 3 right now because we want to be sure we get into the next block.
     return Math.round((data[0].feeRange[0] + MIN_FEE_BUFFER) * 10) / 10
