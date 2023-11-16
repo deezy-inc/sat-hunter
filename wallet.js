6136ce543ae0419e54ba10a4e4d27da5604e08b7
Danny Diekroeger <dannydiek@gmail.com>
2023-11-16 15:15:57 -0800

    Stream response of address transactions

--- wallet.js
+++ wallet.js
@@ -148,16 +148,27 @@ async function broadcast_transaction({ hex }) {
 
 async function get_address_txs({ address }) {
     const url = `${MEMPOOL_API}/address/${address}/txs`
-    const { data } = await axios.get(url, {
-        maxContentLength: Math.MAX_SAFE_INTEGER,
+    // We stream the data because it's often very big.
+    const resp = await axios.get(url, {
+        responseType: 'stream'
     }).catch(err => {
         console.error(err)
         return {}
     })
-    if (process.env.DEBUG) {
-        console.log(data)
+    const stream = resp.data
+    let data = ''
+    let finished = false
+    stream.on('data', (chunk) => {
+        data += chunk.toString()
+    })
+    stream.on('end', () => {
+        finished = true
+    })
+    while (!finished) {
+        await new Promise(resolve => setTimeout(resolve, 50))
     }
-    return data || []
+    if (!data) return []
+    return JSON.parse(data)
 }
 
 async function fetch_most_recent_unconfirmed_send() {
