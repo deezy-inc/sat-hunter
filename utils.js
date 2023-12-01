3ac39befec2b4799fd404cb3f8c76a507f358149
topether21 <topether21@gmail.com>
2023-12-01 11:25:56 -0600

    chore: codecov

--- utils.js
+++ utils.js
@@ -19,13 +19,12 @@ function get_excluded_tags({ fee_rate }) {
         .split(' ')
         .map((tag) => tag.split('/'))
 }
-
 function get_min_tag_sizes({ fee_rate }) {
     let configured_min_tag_sizes = process.env.MIN_TAG_SIZES
-    if (process.env.MIN_TAG_SIZES_HIGH_FEE_THRESHOLD && fee_rate > parseFloat(process.env.MIN_TAG_SIZES_HIGH_FEE_THRESHOLD)) {
+    if (process.env.MIN_TAG_SIZES_HIGH_FEE_THRESHOLD && fee_rate >= parseFloat(process.env.MIN_TAG_SIZES_HIGH_FEE_THRESHOLD)) {
         console.log(`Using high fee min tag sizes`)
         configured_min_tag_sizes = process.env.MIN_TAG_SIZES_HIGH_FEE
-    } else if (process.env.MIN_TAG_SIZES_MEDIUM_FEE_THRESHOLD && fee_rate > parseFloat(process.env.MIN_TAG_SIZES_MEDIUM_FEE_THRESHOLD)) {
+    } else if (process.env.MIN_TAG_SIZES_MEDIUM_FEE_THRESHOLD && fee_rate >= parseFloat(process.env.MIN_TAG_SIZES_MEDIUM_FEE_THRESHOLD)) {
         console.log(`Using medium fee min tag sizes`)
         configured_min_tag_sizes = process.env.MIN_TAG_SIZES_MEDIUM_FEE
     }
@@ -36,11 +35,18 @@ function get_min_tag_sizes({ fee_rate }) {
         const [tag, size] = tagSize.trim().split(':');
         acc[tag] = parseInt(size);
         return acc;
-      }, {});
+    }, {});
 }
 
-function get_included_tags() {
+function get_included_tags({ fee_rate }) {
     let configured_included_tags = process.env.INCLUDE_TAGS
+    if (process.env.INCLUDE_TAGS_HIGH_FEE_THRESHOLD && fee_rate > parseFloat(process.env.INCLUDE_TAGS_HIGH_FEE_THRESHOLD)) {
+        console.log(`Using high fee included tags`)
+        configured_included_tags = process.env.INCLUDE_TAGS_HIGH_FEE
+    } else if (process.env.INCLUDE_TAGS_MEDIUM_FEE_THRESHOLD && fee_rate > parseFloat(process.env.INCLUDE_TAGS_MEDIUM_FEE_THRESHOLD)) {
+        console.log(`Using medium fee included tags`)
+        configured_included_tags = process.env.INCLUDE_TAGS_MEDIUM_FEE
+    }
     if (!configured_included_tags || configured_included_tags.trim() === '') {
         // Explicitly set empty string means excluded_tags has priority.
         return []
