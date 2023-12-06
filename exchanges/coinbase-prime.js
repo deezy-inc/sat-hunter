df137369d583f25654d8327ecf42aa1c548a9802
tylev <tylermatthewevans@gmail.com>
2023-12-06 14:36:44 -0600

    coinbase prime withdrawals implemented

--- exchanges/coinbase-prime.js
+++ exchanges/coinbase-prime.js
@@ -1,25 +1,33 @@
 const axios = require('axios')
 const crypto = require('crypto')
 const totp = require("totp-generator")
-const BASE_URL = 'https://api.prime.coinbase.com/'
+const CryptoJS = require('crypto-js');
+const uuid = require('uuid');
+
+const BASE_URL = 'https://api.prime.coinbase.com'
 
 let BTC_WALLET_ID = null
 let PORTFOLIO_ID = null
 let ENTITY_ID = null
 
-function create_signature({ path, timestamp, method, body = ''}) {
-    const data = `${timestamp}${method}${path}${body}`
-    const base64_key = Buffer.from(process.env.COINBASE_PRIME_SIGNING_KEY, 'base64')
-    return crypto.createHmac('sha256', base64_key)
-        .update(data)
-        .digest('base64');
+function sign(str, secret) {
+    const hash = CryptoJS.HmacSHA256(str, secret);
+    return hash.toString(CryptoJS.enc.Base64);
 }
+function buildPayload(ts, method, requestPath, body) {
+    return `${ts}${method}${requestPath}${body}`;
+}
+
 function create_headers({ path, timestamp, method, body = '' }) {
+    const strToSign = buildPayload(timestamp, method, path, body);
+    const sig = sign(strToSign, process.env.COINBASE_PRIME_SIGNING_KEY);
+
     return {
+        'Content-Type': 'application/json',
         'X-CB-ACCESS-KEY': process.env.COINBASE_PRIME_ACCESS_KEY,
         'X-CB-ACCESS-PASSPHRASE': process.env.COINBASE_PRIME_API_PASSPHRASE,
         'X-CB-ACCESS-TIMESTAMP': timestamp,
-        'X-CB-ACCESS-SIGNATURE': create_signature({ path, timestamp, method, body })
+        'X-CB-ACCESS-SIGNATURE': sig
     }
 }
 
@@ -38,7 +46,7 @@ async function set_coinbase_ids() {
 }
 
 async function set_btc_wallet_id() {
-    const path = `/v1/portfolios/${PORTFOLIO_ID}/wallets?type=TRADING&symbol=BTC`
+    const path = `/v1/portfolios/${PORTFOLIO_ID}/wallets` // ?type=TRADING&symbol=BTC
     const timestamp = `${Math.floor(Date.now() / 1000)}`
     const method = 'GET'
     const headers = create_headers({ path, timestamp, method })
@@ -60,7 +68,7 @@ async function get_btc_balance() {
     if (!BTC_WALLET_ID) {
         await set_btc_wallet_id()
     }
-    const path = `/v1/portfolios/${PORTFOLIO_ID}/wallets/${BTC_WALLET_ID}`
+    const path = `/v1/portfolios/${PORTFOLIO_ID}/wallets/${BTC_WALLET_ID}/balance`
     const timestamp = `${Math.floor(Date.now() / 1000)}`
     const method = 'GET'
     const headers = create_headers({ path, timestamp, method })
@@ -73,25 +81,32 @@ async function get_btc_balance() {
 }
 
 async function withdraw({ amount_btc }) {
-    throw new Error("NOT IMPLEMENTED")
-    if (!process.env.COINBASE_PRIME_ACCESS_KEY || !process.env.COINBASE_PRIME_SIGNING_KEY) {
-        throw new Error('COINBASE_PRIME_ACCESS_KEY and COINBASE_PRIME_SIGNING_KEY must be set')
+    if (!process.env.COINBASE_PRIME_ACCESS_KEY || !process.env.COINBASE_PRIME_SIGNING_KEY || !process.env.COINBASE_PRIME_API_PASSPHRASE) {
+        throw new Error('COINBASE_PRIME_ACCESS_KEY, COINBASE_PRIME_SIGNING_KEY, and COINBASE_PRIME_API_PASSPHRASE must be set')
     }
     if (!process.env.COINBASE_PRIME_WITHDRAWAL_ADDRESS) {
         throw new Error('COINBASE_WITHDRAW_ADDRESS must be set')
     }
-    if (!BTC_ACCOUNT_ID) {
-        BTC_ACCOUNT_ID = await get_btc_account_id()
+    if (!PORTFOLIO_ID || !ENTITY_ID) {
+        await set_coinbase_ids()
     }
-    const path = `/withdrawals/crypto`
+    if (!BTC_WALLET_ID) {
+        await set_btc_wallet_id()
+    }
+    const path = `/v1/portfolios/${PORTFOLIO_ID}/wallets/${BTC_WALLET_ID}/withdrawals`
     const timestamp = `${Math.floor(Date.now() / 1000)}`
     const method = 'POST'
     const body = {
         amount: amount_btc.toFixed(8),
-        currency: 'BTC',
-        profile_id: BTC_ACCOUNT_ID,
-        crypto_address: process.env.COINBASE_PRIME_WITHDRAWAL_ADDRESS,
-        add_network_fee_to_total: false,
+        currency_symbol: 'BTC',
+        portfolio_id: PORTFOLIO_ID,
+        wallet_id: BTC_WALLET_ID,
+        idempotency_key: uuid.v4(),
+        destination_type: 'DESTINATION_BLOCKCHAIN',
+        blockchain_address: {
+            address: process.env.COINBASE_PRIME_WITHDRAWAL_ADDRESS,
+            account_identifier: 'trezor-sat-hunter'
+        }
     }
     if (process.env.COINBASE_PRIME_TOTP_SECRET) {
         body.two_factor_code = totp(process.env.COINBASE_PRIME_TOTP_SECRET)
