21646c130d58ee6f11351c0cca964ed7f5ab11cd
Hash <serhiy@deezy.io>
2024-02-21 19:45:44 +0400

    Split utils by type

---
+++ utils/io.js
@@ -0,0 +1,10 @@
+const { completed_bulk_transfer_dir } = require('../constants');
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const get_success_scan_address_file = ({ scan_request_id, transfer_message }) =>
+    `Bulk transfer of ${transfer_message} have been completed, results can be viewed at ${completed_bulk_transfer_dir}/${scan_request_id}.json`
+
+module.exports = {
+    sleep,
+    get_success_scan_address_file
+}
