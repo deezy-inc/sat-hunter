0d2608d403dd44f8299e271f382420d9f1df35e7
Hash <serhiy@deezy.io>
2024-02-22 14:11:55 +0400

    Refactor using runtime env var reading

--- wallet.js
+++ wallet.js
@@ -10,13 +10,12 @@ const prompts = require('prompts')
 const { decrypt } = require('./encryption')
 const axios = require('axios')
 const ecc = require('tiny-secp256k1')
-const {BIP32Factory} = require('bip32')
+const { BIP32Factory } = require('bip32')
 const bip32 = BIP32Factory(ecc)
 const bitcoin = require('bitcoinjs-lib')
 const bip39 = require('bip39')
 const { getAddressInfo } = require('bitcoin-address-validation');
 const MEMPOOL_URL = process.env.MEMPOOL_URL || 'https://mempool.space'
-const IGNORE_UTXOS_BELOW_SATS = process.env.IGNORE_UTXOS_BELOW_SATS || 1001
 
 bitcoin.initEccLib(ecc)
 
@@ -29,6 +28,8 @@ let root_hd_node
 let child_hd_node
 let derivation_path
 const toXOnly = pubKey => (pubKey.length === 32 ? pubKey : pubKey.slice(1, 33));
+
+const get_min_sat_utxo_limit = () => process.env.IGNORE_UTXOS_BELOW_SATS || 1001
 
 async function init_wallet() {
     if (WALLET_TYPE === 'local') {
@@ -63,10 +64,10 @@ async function init_wallet() {
         child_xonly_pubkey = toXOnly(child_hd_node.publicKey)
         let address
         if (local_wallet_type === 'p2tr') {
-            const p2tr_derived_info = bitcoin.payments.p2tr({internalPubkey: child_xonly_pubkey})
+            const p2tr_derived_info = bitcoin.payments.p2tr({ internalPubkey: child_xonly_pubkey })
             address = p2tr_derived_info.address
         } else {
-            const p2wpkh_derived_info = bitcoin.payments.p2wpkh({pubkey: child_hd_node.publicKey})
+            const p2wpkh_derived_info = bitcoin.payments.p2wpkh({ pubkey: child_hd_node.publicKey })
             address = p2wpkh_derived_info.address
         }
         if (address !== process.env.LOCAL_WALLET_ADDRESS) {
@@ -90,7 +91,10 @@ async function get_utxos_from_mempool_space({ address }) {
     })
     return data
 }
+
 async function get_utxos() {
+    const IGNORE_UTXOS_BELOW_SATS = get_min_sat_utxo_limit();
+
     if (WALLET_TYPE === 'core') {
         const unspents = listunspent()
         const filtered_unspents = unspents.filter(it => it.amount * 100000000 >= IGNORE_UTXOS_BELOW_SATS)
@@ -155,7 +159,7 @@ async function broadcast_to_mempool_space({ hex }) {
     return data
 }
 
-async function broadcast_to_blockstream({ hex}) {
+async function broadcast_to_blockstream({ hex }) {
     const url = `https://blockstream.info/api/tx`
     const { data } = await axios.post(url, hex, { headers: { 'Content-Type': 'text/plain' } }).catch(err => {
         console.error(err)
@@ -214,7 +218,7 @@ async function fetch_most_recent_unconfirmed_send() {
         // Hacky way to find which ones are ours...
         if (it.status.confirmed) return false
         if (it.vin.length !== 1) return false
-        if (it.vin[0].prevout.value < IGNORE_UTXOS_BELOW_SATS) {
+        if (it.vin[0].prevout.value < get_min_sat_utxo_limit()) {
             console.log(`Ignoring spent dust input of ${it.vin[0].prevout.value} sats`)
             return false
         }
