f1db425f39cae17c27ea86cdf5fe5aca3706d003
Danny Diekroeger <dannydiek@gmail.com>
2024-02-29 09:51:08 -0800

    Use default for count value

--- bitcoin.js
+++ bitcoin.js
@@ -44,9 +44,9 @@ function decodepsbt({ psbt }) {
     return resp
 }
 
-function listtransactions({ label = '"*"', count }) {
+function listtransactions({ label = '"*"', count = 10 }) {
     check_wallet()
-    const resp = JSON.parse(child_process.execSync(`${bitcoin_command} -rpcwallet=${process.env.BITCOIN_WALLET} listtransactions ${label} ${count ? count : '10'}`))
+    const resp = JSON.parse(child_process.execSync(`${bitcoin_command} -rpcwallet=${process.env.BITCOIN_WALLET} listtransactions ${label} ${count}`))
     return resp
 }
 
