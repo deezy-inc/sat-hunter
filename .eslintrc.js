06d3d47d02933767ccb5532fe84b26b27e0a4bfc
Hash <serhiy@deezy.io>
2024-03-04 10:15:15 +0400

    Added Eslint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    plugins: ['prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        indent: ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+        'no-undef': 'error',
+        'no-unused-vars': ['error', { argsIgnorePattern: 'req|res|next|__' }],
+        'no-unused-expressions': ['error', { allowTernary: true }],
+        'prettier/prettier': 'error',
+        eqeqeq: 'warn',
+        'no-return-assign': 'error',
+        'no-useless-concat': 'error',
+        'no-useless-return': 'error',
+        'no-mixed-spaces-and-tabs': 'warn',
+        'space-before-blocks': 'error',
+        'max-len': ['error', { code: 200 }],
+        'no-mixed-operators': 'error',
+        'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+        'no-whitespace-before-property': 'error',
+        'nonblock-statement-body-position': 'error',
+        'object-property-newline': [
+            'error',
+            { allowAllPropertiesOnSameLine: true },
+        ],
+
+        //* ES6
+        'arrow-spacing': 'error',
+        'no-confusing-arrow': 'error',
+        'no-duplicate-imports': 'error',
+        'no-var': 'error',
+        'object-shorthand': ['warn', 'always'],
+        'prefer-const': 'error',
+        'prefer-template': 'warn',
+    },
+    overrides: [
+        {
+            files: ['**/__tests__/**/*.{js,mjs}?(x)'],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+}
