0051827cd86b23396ee7857d56e7799edfe1420b
Danny Diekroeger <dannydiek@gmail.com>
2024-03-19 11:55:11 -0700

    fix success msg

--- scripts/sign-and-send.js
+++ scripts/sign-and-send.js
@@ -4,9 +4,6 @@ const data_file = process.argv[2]
 const bitcoin_core_wallet = process.argv[3]
 
 sign_and_send({ data_file, bitcoin_core_wallet })
-    .then((result) => {
-        console.log(result.message)
-    })
     .catch((error) => {
         console.log(`Sending failed with error: ${error.message}`)
     })
