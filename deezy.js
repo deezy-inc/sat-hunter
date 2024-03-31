46277c9001aad0bf6c9c3b9b3bc296eb0722b8ed
habibitcoin <hi@habibitcoin.com>
2024-03-31 11:57:34 -0400

    Include version in headers

--- deezy.js
+++ deezy.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const packageInfo = require('./package.json')
 
 const BASE_URL = process.env.DEEZY_BASE_URL || 'https://api.deezy.io/v1'
 
@@ -11,7 +12,12 @@ async function post_scan_request(request_body) {
     check_api_key()
     const url = `${BASE_URL}/sat-hunting/scan`
     const { data } = await axios
-        .post(url, request_body, { headers: { 'x-api-token': process.env.DEEZY_API_KEY } })
+        .post(url, request_body, {
+            headers: {
+                'x-api-token': process.env.DEEZY_API_KEY,
+                'User-Agent': `sat-hunter/${packageInfo.version}`, // Include the app version
+            },
+        })
         .catch((err) => {
             console.error(err.data)
             return { data: {} }
@@ -22,20 +28,34 @@ async function post_scan_request(request_body) {
 async function get_scan_request({ scan_request_id }) {
     check_api_key()
     const url = `${BASE_URL}/sat-hunting/scan/${scan_request_id}`
-    const { data } = await axios.get(url, { headers: { 'x-api-token': process.env.DEEZY_API_KEY } }).catch((err) => {
-        console.error(err.data)
-        return { data: {} }
-    })
+    const { data } = await axios
+        .get(url, {
+            headers: {
+                'x-api-token': process.env.DEEZY_API_KEY,
+                'User-Agent': `sat-hunter/${packageInfo.version}`, // Include the app version
+            },
+        })
+        .catch((err) => {
+            console.error(err.data)
+            return { data: {} }
+        })
     return data
 }
 
 async function get_user_limits() {
     check_api_key()
     const url = `${BASE_URL}/sat-hunting/user/limits`
-    const { data } = await axios.get(url, { headers: { 'x-api-token': process.env.DEEZY_API_KEY } }).catch((err) => {
-        console.error(err)
-        return { data: {} }
-    })
+    const { data } = await axios
+        .get(url, {
+            headers: {
+                'x-api-token': process.env.DEEZY_API_KEY,
+                'User-Agent': `sat-hunter/${packageInfo.version}`, // Include the app version
+            },
+        })
+        .catch((err) => {
+            console.error(err)
+            return { data: {} }
+        })
     return data
 }
 
