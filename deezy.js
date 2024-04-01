7b83febaff048c1ee7531cc5a26376660cd84eb4
habibitcoin <hi@habibitcoin.com>
2024-04-01 15:04:06 -0400

    Revert interceptor usage

--- deezy.js
+++ deezy.js
@@ -3,54 +3,59 @@ const packageInfo = require('./package.json')
 
 const BASE_URL = process.env.DEEZY_BASE_URL || 'https://api.deezy.io/v1'
 
-// Create an Axios instance
-const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-})
-
-// Use request interceptor to set headers
-axiosInstance.interceptors.request.use(
-    (config) => {
-        // Check API key
-        if (!process.env.DEEZY_API_KEY) {
-            throw new Error('DEEZY_API_KEY must be set')
-        }
-
-        // Set headers
-        config.headers['x-api-token'] = process.env.DEEZY_API_KEY
-        config.headers['User-Agent'] = `sat-hunter/${packageInfo.version}`
-
-        return config
-    },
-    (error) => {
-        return Promise.reject(error)
+function check_api_key() {
+    if (!process.env.DEEZY_API_KEY) {
+        throw new Error('DEEZY_API_KEY must be set')
     }
-)
-
+}
 async function post_scan_request(request_body) {
-    const url = `/sat-hunting/scan`
-    const { data } = await axiosInstance.post(url, request_body).catch((err) => {
-        console.error(err.data)
-        return { data: {} }
-    })
+    check_api_key()
+    const url = `${BASE_URL}/sat-hunting/scan`
+    const { data } = await axios
+        .post(url, request_body, {
+            headers: {
+                'x-api-token': process.env.DEEZY_API_KEY,
+                'User-Agent': `sat-hunter/${packageInfo.version}`, // Include the app version
+            },
+        })
+        .catch((err) => {
+            console.error(err.data)
+            return { data: {} }
+        })
     return data
 }
 
 async function get_scan_request({ scan_request_id }) {
-    const url = `/sat-hunting/scan/${scan_request_id}`
-    const { data } = await axiosInstance.get(url).catch((err) => {
-        console.error(err.data)
-        return { data: {} }
-    })
+    check_api_key()
+    const url = `${BASE_URL}/sat-hunting/scan/${scan_request_id}`
+    const { data } = await axios
+        .get(url, {
+            headers: {
+                'x-api-token': process.env.DEEZY_API_KEY,
+                'User-Agent': `sat-hunter/${packageInfo.version}`, // Include the app version
+            },
+        })
+        .catch((err) => {
+            console.error(err.data)
+            return { data: {} }
+        })
     return data
 }
 
 async function get_user_limits() {
-    const url = `/sat-hunting/user/limits`
-    const { data } = await axiosInstance.get(url).catch((err) => {
-        console.error(err)
-        return { data: {} }
-    })
+    check_api_key()
+    const url = `${BASE_URL}/sat-hunting/user/limits`
+    const { data } = await axios
+        .get(url, {
+            headers: {
+                'x-api-token': process.env.DEEZY_API_KEY,
+                'User-Agent': `sat-hunter/${packageInfo.version}`, // Include the app version
+            },
+        })
+        .catch((err) => {
+            console.error(err)
+            return { data: {} }
+        })
     return data
 }
 
